37be3191d10971215d65246a6d88025bd6d97c53
weeks6 <bridcarp1@gmail.com>
2021-07-18 04:07:49 +0300

    bitching

--- backend/app.js
+++ backend/app.js
@@ -54,6 +54,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post(
   '/signin',
   celebrate({
