53e6ac7b71d66279edb19d09bd61f4db7e0952db
weeks6 <bridcarp1@gmail.com>
2021-07-18 03:12:18 +0300

    maybe fixed cors

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ const { login, createUser } = require('./controllers/user');
 const validateURL = require('./utils/validateURL');
 const NotFoundError = require('./errors/not-found-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { ALLOWED_CORS } = require('./utils/constants');
 
 const app = express();
 app.use(cookieParser());
@@ -27,6 +28,33 @@ mongoose.connect(DB_CONNECTION, {
 });
 
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (ALLOWED_CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
+
+  next();
+});
 
 app.post(
   '/signin',
--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -2,11 +2,19 @@ const ERROR_CODE = 400;
 const NOT_FOUND_CODE = 404;
 const SERVER_ERR_CODE = 500;
 
-const DEV_SECRET = 'f2578d238f6921375e31bb5fc4fba54f0eebca8d74e275b11c770c8f9ed268a0';
+const DEV_SECRET =
+  'f2578d238f6921375e31bb5fc4fba54f0eebca8d74e275b11c770c8f9ed268a0';
+
+const ALLOWED_CORS = [
+  'localhost:3000',
+  'http://weeks6.nomoredomains.monster',
+  'https://weeks6.nomoredomains.monster',
+];
 
 module.exports = {
   ERROR_CODE,
   NOT_FOUND_CODE,
   SERVER_ERR_CODE,
   DEV_SECRET,
+  ALLOWED_CORS,
 };
