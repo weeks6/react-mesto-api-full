d8a479170caff76c6656bd74c3620858bb7911e0
weeks6 <bridcarp1@gmail.com>
2021-07-18 03:37:24 +0300

    fuck them auth[2]

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -9,12 +9,12 @@ const { JWT_SECRET = DEV_SECRET } = process.env;
 module.exports = (req, res, next) => {
   try {
     const { authorization } = req.headers;
+    console.log(authorization);
 
     if (!!authorization || !authorization.startsWith('Bearer ')) {
       throw new UnauthorizedError('Необходима авторизация');
     }
     const token = authorization.replace('Bearer ', '');
-    console.log(token);
 
     const payload = jwt.verify(token, JWT_SECRET);
     if (!payload) {
